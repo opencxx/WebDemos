6f22a9a4e1d74577ff714e77fec17508305ab5ee
chenjijun <chenjijun_work@outlook.com>
2023-06-28 16:42:41 +0800

    -

--- src/Cms.Admin/Program.cs
+++ src/Cms.Admin/Program.cs
@@ -1,6 +1,27 @@
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+
+var asm = typeof(Cms.UI.Areas.Admin.Controllers.HomeController).Assembly;
+var part = new AssemblyPart(asm);
+builder.Services.AddControllersWithViews()
+  .ConfigureApplicationPartManager(apm => apm.ApplicationParts.Add(part));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.UseRouting();
 
-app.Run();
+app.UseAuthentication();
+
+app.MapAreaControllerRoute(
+  name: "default",
+  areaName: "admin",
+  pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllerRoute(name: "admin", pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+await app.RunAsync();
